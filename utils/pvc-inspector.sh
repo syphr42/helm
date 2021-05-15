b006a1db242e642a6803248a388cf4bf6dd9e4cf
Gregory Moyer <moyerg@syphr.com>
2021-05-14 21:14:55 -0400

    Add utility script to inspect a PVC

---
+++ utils/pvc-inspector.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -eo pipefail
+shopt -s failglob
+
+if [ -z "${1}" ]; then
+  echo "error: no pvc name given"
+  exit 1
+fi
+inspector_pvc_name=${1}
+
+kubectl run pvc-inspector --rm -it --restart Never --image alpine --overrides="
+{
+  \"apiVersion\": \"v1\",
+  \"spec\": {
+    \"containers\": [
+      {
+        \"name\": \"pvc-inspector\",
+        \"image\": \"alpine\",
+        \"command\": [
+          \"ash\"
+        ],
+        \"volumeMounts\": [
+          {
+            \"name\": \"data\",
+            \"mountPath\": \"/data\"
+          }
+        ],
+        \"stdin\": true,
+        \"tty\": true
+      }
+    ],
+    \"volumes\": [
+      {
+        \"name\": \"data\",
+        \"persistentVolumeClaim\": {
+          \"claimName\": \"${inspector_pvc_name}\"
+        }
+      }
+    ]
+  }
+}"
